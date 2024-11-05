33e0943c67587f6264a35fca56b9453dd284796c
pranavacchu <pranavacharya360@gmail.com>
2024-11-05 12:07:11 +0530

    vercel7

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/',
@@ -9,27 +9,32 @@ export default defineConfig({
         main: 'index.html',
         start: 'start.html',
         game: 'game.html',
-        game2: 'game2.html',
+        game2: 'game2.html'
       },
       output: {
         assetFileNames: (assetInfo) => {
           // Keep .glb files in root
           if (assetInfo.name.endsWith('.glb')) {
-            return '[name][extname]';
+            return '[name][extname]'
           }
           // Other assets go to assets directory
-          return 'assets/[name]-[hash][extname]';
+          return 'assets/[name]-[hash][extname]'
         },
-      },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
+      }
     },
     sourcemap: true,
     assetsInclude: ['**/*.glb'],
-    assetsInlineLimit: 0,
+    assetsInlineLimit: 4096
   },
   server: {
+    proxy: {
+      '/ws': 'http://localhost:8000'
+    },
     fs: {
       strict: false,
-      allow: ['.'],
-    },
-  },
-});
+      allow: ['.']
+    }
+  }
+})
