ece880e8c1739a4002db267f257e4c7b9e3df855
pranavacchu <pranavacharya360@gmail.com>
2024-11-05 22:36:42 +0530

    firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,23 +1,38 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBeg5sot6EJWp9NpEFrllEK1YJ9lZlGKdI",
+//   authDomain: "battleship-dd5eb.firebaseapp.com",
+//   projectId: "battleship-dd5eb",
+//   storageBucket: "battleship-dd5eb.firebasestorage.app",
+//   messagingSenderId: "611534322110",
+//   appId: "1:611534322110:web:07234ee97afada294f61ef",
+//   measurementId: "G-1TLK27C94J"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBeg5sot6EJWp9NpEFrllEK1YJ9lZlGKdI",
-  authDomain: "battleship-dd5eb.firebaseapp.com",
-  projectId: "battleship-dd5eb",
-  storageBucket: "battleship-dd5eb.firebasestorage.app",
-  messagingSenderId: "611534322110",
-  appId: "1:611534322110:web:07234ee97afada294f61ef",
-  measurementId: "G-1TLK27C94J"
+  apiKey: "AIzaSyD4x1iN0TiNqQXaAUdRcNaXyGO_TtM6v6U",
+  authDomain: "battleship-arvr.firebaseapp.com",
+  projectId: "battleship-arvr",
+  storageBucket: "battleship-arvr.firebasestorage.app",
+  messagingSenderId: "261166896789",
+  appId: "1:261166896789:web:57b63b7c84e9c868706d9c",
+  measurementId: "G-YENFN0VEZD"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+// Anonymous sign-in for Firebase auth
+signInAnonymously(auth).catch(error => {
+  console.error("Error during anonymous sign-in:", error);
+});
+
+export { database };
